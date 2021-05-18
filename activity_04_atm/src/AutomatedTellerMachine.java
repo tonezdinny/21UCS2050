ee1b46142e7224c6e780345aea20df4bff1e79ff
Thyago Mota <tmota@msudenver.edu>
2021-05-18 11:42:53 -0600

    updates

---
+++ activity_04_atm/src/AutomatedTellerMachine.java
@@ -0,0 +1,10 @@
+/*
+ * CS2050 - Computer Science II - Summer 2021
+ * Instructor: Thyago Mota
+ * Description: Activity 04 - AutomatedTellerMachine
+ */
+
+public class AutomatedTellerMachine {
+
+
+}
---
+++ activity_04_atm/src/AutomatedTellerMachineDriver.java
@@ -0,0 +1,14 @@
+/*
+ * CS2050 - Computer Science II - Summer 2021
+ * Instructor: Thyago Mota
+ * Description: Activity 04 - AutomatedTellerMachineDriver
+ */
+
+import java.util.Random;
+
+public class AutomatedTellerMachineDriver {
+
+    public static void main(String[] args) {
+
+    }
+}
