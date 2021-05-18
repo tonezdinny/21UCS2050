b5ccbf635b65de6b585165b0e882507c4c38fe29
Thyago Mota <tmota@msudenver.edu>
2021-05-18 14:07:58 -0600

    updates

---
+++ activity_18_selection_sort/src/SelectionSort.java
@@ -0,0 +1,24 @@
+/*
+ * CS 2050 - Computer Science II - Summer 2021
+ * Instructor: Thyago Mota
+ * Description: Activity 18 - Selection Sort
+ */
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class SelectionSort {
+
+    // TODO: implement the selection sort algorithm
+    public static void selectionSort(int data[]) {
+
+    }
+
+    public static void main(String[] args) {
+
+        int data[] = {13, 12, 84, 79, 10, 77, 56, 1, 34, 27, 3};
+        System.out.println(Arrays.toString(data));
+        selectionSort(data);
+        System.out.println(Arrays.toString(data));
+    }
+}
