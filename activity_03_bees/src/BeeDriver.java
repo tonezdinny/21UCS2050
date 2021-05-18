49d073f1b3882800c444142f8f8232cfc630dd11
Thyago Mota <tmota@msudenver.edu>
2021-05-18 13:45:24 -0600

    updates

---
+++ activity_03_bees/src/BeeDriver.java
@@ -0,0 +1,16 @@
+/*
+ * CS2050 - Computer Science II - Summer 2021
+ * Instructor: Thyago Mota
+ * Description: Activity 03 - BeeDriver
+ */
+
+import java.util.Random;
+
+public class BeeDriver {
+
+    public static void main(String[] args) {
+        final int TOTAL_BEES = 15;
+        
+
+    }
+}
