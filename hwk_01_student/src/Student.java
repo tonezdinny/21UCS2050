a5aba660f12c4622e3c6ea69f01af1bce71b264b
Thyago Mota <tmota@msudenver.edu>
2021-06-03 11:55:16 -0600

    updates

---
+++ hwk_01_student/src/Student.java
@@ -0,0 +1,16 @@
+/*
+ * CS2050 - Computer Science II - Summer 2021
+ * Instructor: Thyago Mota
+ * Description: Hwk 01 - Student
+ */
+
+public class Student {
+
+    // TODO #1: define 3 instance variables to hold id, name, and major
+
+    // TODO #2: write a constructor with all 3 parameters
+
+    // TODO #3: write a constructor with only the student's id and name (in that case, major should be set to "undeclared")
+
+    // TODO #4: write a method called getMajor that returns the major of the student
+}
---
+++ hwk_01_student/src/StudentDriver.java
@@ -0,0 +1,15 @@
+/*
+ * CS2050 - Computer Science II - Summer 2021
+ * Instructor: Thyago Mota
+ * Description: Hwk 01 - StudentDriver
+ */
+
+public class StudentDriver {
+
+    public static void main(String[] args) {
+
+        // TODO #5: instantiate a student object
+
+        // TODO #6: display the student's major
+    }
+}
