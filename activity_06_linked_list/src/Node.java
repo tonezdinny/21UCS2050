3e485c598c99bf5040570ea00fd57ac8bfc82938
Thyago Mota <tmota@msudenver.edu>
2021-06-16 11:10:48 -0600

    updates

--- activity_06_linked_list/src/Node.java
+++ activity_06_linked_list/src/Node.java
@@ -5,4 +5,33 @@
  */
 
 public class Node {
+
+    private int  value;
+    private Node next;
+
+    public Node(int value) {
+        this.value = value;
+        next = null;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return value + "";
+    }
 }
